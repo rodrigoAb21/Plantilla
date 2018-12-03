90f052950d2046d0238e44bbb2500e489c775c75
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-12-03 03:29:08 -0400

    bitacora archivos

---
+++ inegas/zzzz/Accion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Accion extends Model
+{
+    protected $table = 'accion';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'accion',
+        'fecha',
+        'tabla',
+        'tupla',
+        'idBitacora',
+    ];
+}
