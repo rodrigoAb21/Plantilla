fbf1e6dd741eba41d11aa45dd88609a26abbc9f0
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-12-06 04:06:07 -0400

    Revaluo Version Nath
    +10$us jajaja

--- inegas/resources/views/activos/revaluos/create.blade.php
+++ inegas/resources/views/activos/revaluos/create.blade.php
@@ -15,25 +15,41 @@
                     {{csrf_field()}}
                         <div class="row">
 
-                                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label for="activo">Codigo/Grupo/Costo</label>
-                                        <select class="form-control selectpicker" data-live-search="true" data-style="btn btn-link" name="activo_fijo_id">
-                                            @foreach($activos as $activo)
-                                                <option value="{{$activo -> id}}">{{$activo -> codigo.' / '.$activo -> grupo.' / '.$activo -> costo_actual.'Bs'}}</option>
-                                            @endforeach
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                <div class="form-group">
+                                    <label for="activo">Codigo / Grupo</label>
+                                    <select id="selector" class="form-control selectpicker" data-live-search="true" data-style="btn btn-link" name="activo_fijo_id">
+                                        @foreach($activos as $activo)
+                                            <option value="{{$activo -> id}}">{{$activo -> codigo.' / '.$activo -> grupo}}</option>
+                                        @endforeach
 
-                                        </select>
-                                    </div>
+                                    </select>
                                 </div>
-                                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                                    <div class="form-group mt-2">
-                                        <div class="mb-1">
-                                            <label>Monto Bs</label>
-                                        </div>
-                                        <input type="number" step="any" class="form-control" name="monto">
+                            </div>
+                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                <div class="form-group mt-2">
+                                    <div class="mb-1">
+                                        <label>Costo Actual Bs</label>
                                     </div>
+                                    <p class="form-control" id="costo_actual"></p>
                                 </div>
+                            </div>
+                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                <div class="form-group mt-2">
+                                    <div class="mb-1">
+                                        <label>Monto Bs</label>
+                                    </div>
+                                    <input id="monto" type="number" onblur="calcular_costo_final()" step="any" class="form-control" name="monto" value="0">
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                <div class="form-group mt-2">
+                                    <div class="mb-1">
+                                        <label>Costo Final Bs</label>
+                                    </div>
+                                    <input type="number" onblur="calcular_monto()" id="costo_final" step="any" class="form-control">
+                                </div>
+                            </div>
 
                             </div>
                             <div class="row">
@@ -42,11 +58,10 @@
                                         <div class="mb-1">
                                             <label>Motivo</label>
                                         </div>
-                                        <textarea rows="3" class="form-control" name="motivo"></textarea>
+                                        <textarea rows="3" class="form-control" name="motivo" required></textarea>
                                     </div>
                                 </div>
                             </div>
-
 
                 </div>
                 <div class="card-footer">
@@ -61,4 +76,54 @@
         <!-- end col-md-12 -->
     </div>
     <!-- end row -->
+    @push('scripts')
+        <script>
+            var activos = [];
+            var costo_actual = 0;
+            var costo_final = 0;
+            var monto = 0;
+            $(document).ready(
+                function () {
+                    @foreach($activos as $activo)
+                    activos.push({id:parseInt('{{$activo -> id}}'), costo_actual:parseFloat('{{$activo -> costo_actual}}')})
+                    @endforeach
+
+                    seleccionado();
+                }
+            );
+
+            $('#selector').change(seleccionado);
+
+
+            function seleccionado() {
+                var selected = $('#selector').val();
+                activos.forEach(function (activo) {
+                    if (activo.id == selected){
+                        costo_actual = activo.costo_actual;
+                        $('#costo_actual').html(costo_actual);
+                    }
+                })
+                calcular_costo_final();
+            }
+
+            function calcular_monto() {
+                costo_final = parseFloat($('#costo_final').val());
+                monto = costo_final - costo_actual;
+                $('#monto').val(monto);
+            }
+
+            function calcular_costo_final() {
+                monto = parseFloat($('#monto').val());
+                costo_final = costo_actual + monto;
+                $('#costo_final').val(costo_final);
+            }
+
+
+
+
+
+        </script>
+    @endpush
+
+
 @endsection
