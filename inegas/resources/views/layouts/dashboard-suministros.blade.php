801b07e1f46dd9b4ddea6d957c74edc11b652900
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-12-08 04:36:04 -0400

    Icono buscar

--- inegas/resources/views/layouts/dashboard-suministros.blade.php
+++ inegas/resources/views/layouts/dashboard-suministros.blade.php
@@ -144,7 +144,15 @@
                     <span class="navbar-toggler-icon icon-bar"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end">
-                    <div class="navbar-form" hidden></div>
+                    <form class="navbar-form">
+                        <div class="input-group no-border">
+                            <input type="text" value="" class="form-control" placeholder="Buscar...">
+                            <button type="button" class="btn btn-white btn-round btn-just-icon">
+                                <i class="material-icons">search</i>
+                                <div class="ripple-container"></div>
+                            </button>
+                        </div>
+                    </form>
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('config/tema')}}">
